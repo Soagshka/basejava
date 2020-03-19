ed72bdcf691b96a59540eb14f453d3db408b1da1
Soagshka <40063763+Soagshka@users.noreply.github.com>
2020-03-19 13:51:57 +0300

    Changed ArrayStorage, changed modifier for ARRAY_STORAGE in MainArray

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,51 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int i = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        i = 0;
     }
 
     void save(Resume r) {
+        storage[i] = r;
+        i++;
     }
 
     Resume get(String uuid) {
+        for (int j = 0; j < i; j++) {
+            if (uuid.equals(storage[j].uuid)) {
+                return storage[j];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int j = 0; j < i; j++) {
+            if (storage[j].uuid.equals(uuid)) {
+                storage[j] = null;
+                for (int k = 0; k < i; k++) {
+                    storage[k] = storage[k + 1];
+                }
+                i--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, i);
     }
 
     int size() {
-        return 0;
+        return i;
     }
 }
