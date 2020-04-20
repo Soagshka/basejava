e0b65f824b9e47baa6795085e79e9e7c0feb7cac
Soagshka <alexandergorbenko1996@gmail.com>
2020-04-20 23:36:26 +0300

    Lesson 09 HW. Changed MainFile

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -4,8 +4,6 @@ import java.io.File;
 
 public class MainFile {
 
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) {
         File dir = new File("./src");
         printFiles(dir, "");
@@ -16,17 +14,13 @@ public class MainFile {
         if (files != null) {
             for (File file : files) {
                 if (file.isDirectory()) {
-                    System.out.println(sb.append("\t") + "Folder " + file.getName() + " : ");
-                    printFiles(file, sb.toString());
+                    System.out.println(tabs + "Folder " + file.getName() + " : ");
+                    printFiles(file, tabs + "\t");
                 }
 
                 if (file.isFile()) {
                     System.out.println(tabs + file.getName());
                 }
-            }
-            int i = sb.indexOf("\t");
-            if (i != -1) {
-                sb.delete(i, i + "\t".length());
             }
         }
     }
