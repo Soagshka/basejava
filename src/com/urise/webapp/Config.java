68b0a8ba5100423204bfeb74fce7a676ca72575f
Soagshka <40063763+Soagshka@users.noreply.github.com>
2020-05-15 16:50:01 +0300

    Lesson17 HW.

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -1,16 +1,14 @@
 package com.urise.webapp;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("E:\\basejava\\config\\resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
 
-    private Properties props = new Properties();
     private File storageDir;
 
     private String dbUrl;
@@ -22,14 +20,15 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
+            Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             dbUrl = props.getProperty("db.url");
             dbUser = props.getProperty("db.user");
             dbPassword = props.getProperty("db.password");
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS);
         }
     }
 
